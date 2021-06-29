f3ef92df3fee9578c9f3065111932d908f2399a6
mikael <mikaeldalves@gmail.com>
2021-06-29 12:07:45 -0300

    att manhã de segunda

--- principal.py
+++
@@ -1,7 +0,0 @@
-import conjuntoDominanteMinimo as CDM
-
-cdm = CDM.DDconjuntoDominanteMinimo()
-t = cdm.encontrarNoOtimo()
-print(t.custo)
-print(t.solParcial)
-print(t.estado)
--- testeMetodosGrafo.py
+++ testeMetodosGrafo.py
@@ -1,44 +1,41 @@
 import numpy as np
-	
-
-class Grafo:
-	def __init__(self):
-		self.grafo = self.lergrafo() #Matriz do grafo 
-		self.pesos = self.lerpesos() #pesos dos vértices
 
 
-
-	def lergrafo(self):
+def lergrafo():
 		try:
 			file = open("instancia1.txt", "rt")
-		
+			
 		except:
-			print("Erro ao ler o arquivo.")
+			print("Erro ao ler o arquivo")
 			file.close()
 
 		else:
 			x = int(file.readline())
 			grafo = np.zeros((x,x)) #Cria uma matriz de tamanho NxN
-			for i in range(int(file.readline())): #for percorendo o número de arestas e prenchendo a matriz
+			for i in range(int(file.readline())): #for percorendo o número de arestas 
 				x = file.readline()
 				x = x.split(" ")
 				x = [int(x) for x in x]
 				grafo[x[0] - 1, x[1] - 1] = 1
-				grafo[x[1] - 1, x[0] - 1] = 1
+				grafo[x[1] - 1, x[0] - 1] = 1	
 			file.close()
+			print(grafo)
 			return grafo
 
-
-	def lerpesos(self):
+def lerpesos():
 		try:
 			file = open("instancia1.txt", "rt")
 
 		except:
 			print("Erro ao ler o arquivo.")
 			file.close()
-
+			
 		else:
 			x = int(file.readline())
-			pesos = np.ones(x) #Vetor de tamanho N com somente 1's.
+			pesos = np.ones(x)
 			file.close()
+			print(pesos)
 			return pesos
+
+x = lergrafo()
+x = lerpesos()
