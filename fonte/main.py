86ed43a46d8397c7301380f21df8ebc4dc67f3bf
caioemanuel <caioemanuel@noar.tv>
2021-07-05 13:18:51 -0300

    comentário com instruções p rodar o cod

--- fonte/main.py
+++ fonte/main.py
@@ -4,5 +4,11 @@ cdm = CDM.DDconjuntoDominanteMinimo()
 listaDeNos = cdm.encontrarNoOtimo()
 
 print("custo:", listaDeNos[0].custo)
+
 for t in listaDeNos:
-	print(t.solParcial)
+    print(t.solParcial)
+
+# Instruções para rodar o código:
+# Instalar a biblioteca "numpy" EM "Ferramentas" -> "Gerenciar Pacotes" ->
+# -> digitar "numpy" no campo de testo e clicar em pesquisar ->
+# clicar na primeira versão e depois em "instalar"
