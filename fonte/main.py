227e3530d4c12a1cb24b2df1288fbf34425297bd
mikael <mikaeldalves@gmail.com>
2021-07-05 14:50:35 -0300

    adicionado comentarios main

--- fonte/main.py
+++ fonte/main.py
@@ -7,3 +7,9 @@ print("Custo:", listaDeNos[0].custo, "\n")
 print("Conjuntos solucao otimo:")
 for t in listaDeNos:
 	print(t.solParcial)
+
+
+# Instruções para rodar o código:
+# Instalar a biblioteca "numpy" EM "Ferramentas" -> "Gerenciar Pacotes" ->
+# -> digitar "numpy" no campo de testo e clicar em pesquisar ->
+# clicar na primeira versão e depois em "instalar"
